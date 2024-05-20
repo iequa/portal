ab2d00d6186fdd43cee12a0e665c5173fef3d7df
klyksamax <maxholod2@gmail.com>
2024-05-20 19:56:31 +0300

    Добавлено закрытие popup

--- src/Components/PopupMenu/PopupMenu.js
+++ src/Components/PopupMenu/PopupMenu.js
@@ -12,16 +12,26 @@ const PopupMenu = observer(({ popupStore}) => {
         setIsOpen(popupStore.openPopUp)
     },[popupStore.openPopUp])
 
-    console.log(!popupStore.openPopUp)
+    function openClosePopUp (e) {
+      const popup = document.querySelector('.popup-menu__container')
+      if (e.target.className === popup.className) {
+        ClosePopUp()
+      }
+    }
+
+    function ClosePopUp (parameters) {
+      popupStore.setOpenPopUp(false)
+    }
 
     if(!popupStore.openPopUp)
     return false
 
     return (
-      <div  className="popup-menu__container">
+      <div  className="popup-menu__container" onClick={(e)=>{openClosePopUp(e)}}>
         <div className="popup-menu__block">
         <header className="popup-menu__block_header">
             <h2>{popupStore.title}</h2>
+            {popupStore.crose ? <div className="popup-menu__crose" onClick={ClosePopUp}>SVG</div> : null}
             </header>
         <main>{popupStore.main}</main>
         <footer className="popup-menu__block_footer">
